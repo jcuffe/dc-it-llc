c5d89e5ddb44a7556c087f5ddb238240ffa1e59d
Jared Cuffe <jcuffe@gmail.com>
2019-01-26 20:47:33 -0800

    Adds Roboto fonts to the root document.

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import CSG from "./csg";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CSG />
+      </div>
+    );
+  }
+}
+
+export default App;
