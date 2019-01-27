316c4b3f9bc71b0fd29a239ada7764c8a71021ec
Jared Cuffe <jcuffe@gmail.com>
2019-01-27 11:00:38 -0800

    Wraps app in state provider, and injects Material CSS.

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { Provider } from "./state/store";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const app = (
+  <Provider>
+    <CssBaseline />
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
