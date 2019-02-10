1b4e7a9275135cb2ca8a043ab785870c443f4f46
Jared Cuffe <jcuffe@gmail.com>
2019-02-09 17:06:58 -0800

    Provides navigation bar.

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+
+const styles = {
+  appBar: {
+    marginBottom: 10,
+    paddingLeft: 50,
+    paddingRight: 50
+  },
+  toolBar: {
+    display: "flex",
+    justifyContent: "space-evenly"
+  },
+  navItem: {
+    color: "white",
+    textDecoration: "none",
+    "&:hover": {
+      color: "grey"
+    }
+  }
+};
+
+const Navbar = ({ classes }) => (
+  <AppBar position="static" className={classes.appBar}>
+    <Toolbar className={classes.toolBar}>
+      <Typography variant="h6">
+        <Link to="/" className={classes.navItem}>
+          Process Payments
+        </Link>
+      </Typography>
+      <Typography variant="h6">
+        <Link to="/processed" className={classes.navItem}>
+          View Processed Payments
+        </Link>
+      </Typography>
+      <Typography variant="h6">
+        <Link to="/latest-response" className={classes.navItem}>
+          Latest BillingTree Response
+        </Link>
+      </Typography>
+    </Toolbar>
+  </AppBar>
+);
+
+export default withStyles(styles)(Navbar);
