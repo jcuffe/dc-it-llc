884af3eed0666128339dcf7d7739f1b6b36b90a5
Jared Cuffe <jcuffe@gmail.com>
2019-02-09 17:13:04 -0800

    Implements a view for client to examine BillingTree response.

---
+++ client/src/components/BillingTreeLatest.js
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { Paper, Grid, Typography } from "@material-ui/core";
+import { Store } from "../state/store";
+import DataTable from "./DataTable";
+
+const paperStyle = { height: "80vh", width: 1270, paddingTop: 10 };
+
+const BillingTreeLatest = () => {
+  const { state, dispatch } = useContext(Store);
+  let columns, rows;
+  if (state.billingTreeRows.length) {
+    rows = state.billingTreeRows;
+    columns = Object.keys(rows[0]).map((key, i) => {
+      const widths = [200, 350, 420, 300];
+      return {
+        width: widths[i],
+        label: key,
+        dataKey: key
+      };
+    });
+  } else {
+    columns = [];
+    rows = [];
+  }
+
+  return (
+    <Grid container justify="center">
+      <Grid item>
+        <Typography gutterBottom variant="headline" align="center">
+          Latest BillingTree Response
+        </Typography>
+        <Paper style={paperStyle}>
+          <DataTable columns={columns} rows={rows} onRowClick={null} />
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default BillingTreeLatest;
