703af76cc510109baf68fa902df66bf32b85e753
Jared Cuffe <jcuffe@gmail.com>
2019-02-09 17:05:37 -0800

    Expands component to encompass multiple routes.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,21 @@
 import React, { useContext } from "react";
 import { Grid } from "@material-ui/core";
-import { Store } from "../state/store";
-import CSG from "./csg";
+import { Switch, Route } from "react-router";
+import Navbar from "./Navbar";
+import ProcessPayments from "./ProcessPayments";
+import Processed from "./Processed";
+import BillingTreeLatest from "./BillingTreeLatest";
 
 const App = () => {
   return (
-    <Grid container justify="center">
-      <CSG />
-    </Grid>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProcessPayments} />
+        <Route path="/processed" component={Processed} />
+        <Route path="/latest-response" component={BillingTreeLatest} />
+      </Switch>
+    </div>
   );
 };
 
