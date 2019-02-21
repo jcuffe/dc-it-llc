410c1815effaea8d7d4fbe654aef30587528bc53
Jared Cuffe <jcuffe@gmail.com>
2019-02-20 17:36:54 -0800

    Configures secure ftp client.

---
+++ server/util/sftp.js
@@ -0,0 +1,28 @@
+const Client = require("ssh2-sftp-client");
+const sftp = new Client();
+
+const options = {
+  host: process.env.FTP_HOST,
+  port: 22,
+  username: process.env.FTP_USER,
+  password: process.env.FTP_PASSWORD,
+  algorithms: {
+    cipher: [
+      'aes128-ctr',
+      'aes192-ctr',
+      'aes256-ctr',
+      'aes128-gcm',
+      'aes128-gcm@openssh.com',
+      'aes256-gcm',
+      'aes256-gcm@openssh.com',
+      'aes256-cbc'
+    ]
+  }
+}
+
+sftp._connect = sftp.connect;
+sftp.connect = function() {
+  return this._connect(options);
+}
+
+module.exports = sftp;
