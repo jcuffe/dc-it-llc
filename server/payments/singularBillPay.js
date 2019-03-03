a265612968ba2a04f995f998053d767cca56d0af
Jared Cuffe <jcuffe@gmail.com>
2019-03-03 13:47:58 -0800

    Adds duplicate check to prevent double billing.

--- server/payments/singularBillPay.js
+++ server/payments/singularBillPay.js
@@ -14,9 +14,21 @@ const batchTransactions = async (client, ids) => {
     transactions.map(async (tx, i) => {
       let result;
       let refnum = null;
-      const response = await runTransaction(client, tx);
-      result = response.responsemessage;
-      refnum = response.transactionreference;
+      const validated = await validateTransaction(client, tx.id);
+      if (validated.length) {
+        result = "Duplicate Transaction."
+      } else {
+        await Processed.insertTransaction(tx);
+        const response = await runTransaction(client, tx);
+        if (response.responsemessage === "success") {
+          result = "Approved";
+          refnum = response.transactionreference;
+          await Processed.finalizeTransaction(tx.id, refnum);
+        } else {
+          result = response.responsemessage;
+          await Processed.removeTransaction(tx.id);
+        }
+      }
       return {
         id: tx.id,
         "Debtor Name": tx["Debtor Name"],
@@ -59,6 +71,19 @@ const runTransaction = async (client, tx) => {
 }
 
 const validateTransaction = async (client, id) => {
+  const args = {
+    partnerkey: "21A47A5F-9992-46E5-A198-58756A38DDAF",
+    partnerid: "pyds",
+    orderid: id
+  }
+  
+  try {
+    const response = await client.V1.getReport({ SbpQueryRequest: args })
+    return response.body;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 }
 
 module.exports = {
